9c5d6a1a541c118c02eaf63df9fcd0c0123010d1
Jet Lee <jettdlee@gmail.com>
2021-12-02 09:36:11 +0000

    day 1 part 2

--- day_1_sonar_sweep/main.py
+++ day_1_sonar_sweep/main.py
@@ -6,6 +6,7 @@ class DepthChecker:
     def __init__(self, dataset):
         self.dataset = np.array(dataset)
         self.count = 0
+        self.countSum = 0
 
     def findIncreaseMeasurement(self):
         for index in range(len(self.dataset)):
@@ -14,10 +15,24 @@ class DepthChecker:
             if self.dataset[index] > self.dataset[index-1]:
                 self.count += 1
 
+    def calcualateSumOfNextDepth(self):
+        for index in range(len(self.dataset)):
+            if index == 0:
+                continue 
+            currentSum = self.__getSumOnIndex(index)
+            previousSum = self.__getSumOnIndex(index-1)
+            if currentSum > previousSum:
+                self.countSum += 1
+
+    def __getSumOnIndex(self, index):
+        return self.dataset[index:index+3].sum()
+
 if __name__ == "__main__":
     data_file = ImportData("day_1.data", True)
     checker = DepthChecker(data_file.dataset)
     checker.findIncreaseMeasurement()
     print(checker.count)
-    
+
+    checker.calcualateSumOfNextDepth()
+    print(checker.countSum)
     
