cadc477904e8051a2061cb00de6d10ec7c5a7738
Jet-Tsyn Lee <jlee@yozu.co.uk>
2021-12-03 10:09:05 +0000

    day 3 part 1

---
+++ day_3_binary_diagnostic/main.py
@@ -0,0 +1,53 @@
+import pdb
+import numpy as np
+from helpers.import_data import ImportData
+
+class PowerThing:
+
+    def __init__(self, dataset):
+        self.dataset = np.array(dataset)
+        self.gamma = 0
+        self.epsilon = 0
+
+    def calculateGammaRate(self):
+        gamma_bits = []
+        for index in range(len(self.dataset[0])):
+            bits_in_index_position = self.__getBitsOnPosition(index) 
+            values, counts = self.__countBits(bits_in_index_position)
+            max_value_index = np.argmax(counts)
+            gamma_bits.append(values[max_value_index])
+        self.gamma = self.__convertBinaryToDecimal(''.join(np.char.mod('%i', gamma_bits)))
+        print('Calculated Gamma: ', self.gamma)
+
+    def calculateEpsilonRate(self):
+        epsilon_bits = []
+        for index in range(len(self.dataset[0])):
+            bits_in_index_position = self.__getBitsOnPosition(index) 
+            values, counts = self.__countBits(bits_in_index_position)
+            min_value_index = np.argmin(counts)
+            epsilon_bits.append(values[min_value_index])
+        self.epsilon = self.__convertBinaryToDecimal(''.join(np.char.mod('%i', epsilon_bits)))
+        print('Calculated Epsilon: ', self.epsilon)
+
+    def calculatePowerConsumption(self):
+        print("Power Consumption: ", self.gamma * self.epsilon)
+
+    def __getBitsOnPosition(self, index):
+        binary_array = []
+        for bits in self.dataset:
+            binary_array.append(int(bits[index]))
+        return np.array(binary_array)
+
+    def __convertBinaryToDecimal(self, binary):
+        return int(binary, 2)
+
+    def __countBits(self, array):
+        return np.unique(array, return_counts=True)
+
+if __name__ == "__main__":
+    data_file = ImportData("day_3.data", False)
+    power = PowerThing(data_file.dataset)
+    power.calculateGammaRate()
+    power.calculateEpsilonRate()
+    power.calculatePowerConsumption()
+    
