259ba7df66ad23b846a971cb7d5c4615149772ed
Jet Lee <jettdlee@gmail.com>
2021-12-07 22:00:05 +0000

    day 7 part 2

--- day_7_the_trechery_of_whales/main.py
+++ day_7_the_trechery_of_whales/main.py
@@ -9,14 +9,16 @@ class CrabFuel:
         self.min_position = min(self.crabs)
         self.max_position = max(self.crabs)
 
-    def run(self):
-        fuel_cost_dict = {}
+    def run(self, efficent_fuel=False):
         fuel_array = []
         for position in range(self.min_position, self.max_position+1):
             fuel_cost = 0
             for crab in self.crabs:
-                fuel_cost += abs(crab - position)
-            # fuel_cost_dict[position] = fuel_cost
+                if efficent_fuel == True:
+                    steps = abs(crab - position)
+                    fuel_cost += sum(i for i in range(1, steps+1))
+                else:
+                    fuel_cost += abs(crab - position)
             fuel_array.append(fuel_cost)
         print(min(fuel_array))
 
@@ -27,3 +29,4 @@ if __name__ == "__main__":
     data_file = ImportData('day_7.data')
     crab_fuel = CrabFuel(data_file.dataset)
     crab_fuel.run()
+    crab_fuel.run(True)
