5eb96db024156f1df0143712c57104dde2d02fa3
Jet Lee <jettdlee@gmail.com>
2021-12-07 19:02:06 +0000

    day 5 wip

---
+++ day_6_lanternfish/main.py
@@ -0,0 +1,38 @@
+import pdb
+import numpy as np
+from helpers.import_data import ImportData
+
+
+class LanternFish():
+    def __init__(self, lifespan=8):
+        self.lifespan = lifespan
+
+    def decrementDay(self):
+        if self.lifespan == 0:
+            self.lifespan = 6
+            return LanternFish()
+        else:
+            self.lifespan -= 1
+
+class FishCounter:
+    def __init__(self, dataset):
+        self.dataset = list(map(int, dataset[0].split(',')))
+
+    def countFish(self, days):
+        fish_array = []
+        for day in range(days +1):
+            for fish in fish_array:
+                new_fish = fish.decrementDay()
+                if new_fish is not None:
+                    fish_array.append(new_fish)
+
+            lifespan = self.dataset[day]
+            fish_array.append(LanternFish(lifespan))
+        print(len(fish_array))
+
+if __name__ == "__main__":
+    data_file = ImportData('day_6.data')
+    fish_counter = FishCounter(data_file.dataset)
+    fish_counter.countFish(80)
+
+    
